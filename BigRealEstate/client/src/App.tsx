72aa7cd6f85d6fd7344749d21ec3d14d4e4cd67a
Colin Tondreau <cdt8588@rit.edu>
2025-09-07 19:14:20 -0400

    Updated UI to be more appealing

--- BigRealEstate/client/src/App.tsx
+++ BigRealEstate/client/src/App.tsx
@@ -1,21 +1,8 @@
 import "./App.css";
+import { NavigationProvider } from "./providers";
 
 function App() {
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center gap-y-4">
-        <p className="text-5xl font-bold">Team: BigRealEstate</p>
-        <p className="text-2xl">
-          By: Robert Huang, Andrew Moulton, Jonathan Zhu, Niccolls Evsseef,
-          Colin Tondreau
-        </p>
-
-        <p className="text-2xl">Coach: Dmitry Lukyanov</p>
-        <p className="text-2xl">Project: ZLA - Real Estate Lead Gen and CRM</p>
-        <p className="text-2xl">Sponsor: Cora sur Los Cabos</p>
-      </div>
-    </>
-  );
+  return <NavigationProvider />;
 }
 
 export default App;
